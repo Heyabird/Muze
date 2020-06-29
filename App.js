dd24acdaf022e348e8719bb1497a4ce1eaf2a60f
heya <heyabird@gmail.com>
2020-06-28 17:33:00 -0700

    made two pages - explore and home'

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
+// source: https://reactnative.dev/docs/navigation
+
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import Home from './Pages/Home';
+import Explore from './Pages/Explore';
+
 import { NavigationContainer } from '@react-navigation/native';
-import Home from './Home'
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home/>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: 'MUZE' }}
+      />
+      <Stack.Screen name="Explore" component={Explore} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
