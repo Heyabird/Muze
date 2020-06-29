771c3df417ad671959134edb9c93203cc5cdaee7
heya <heyabird@gmail.com>
2020-06-28 17:23:49 -0700

    added react-native-navigation and wrapped Home.js inside App.js

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to Muze!</Text>
-    </View>
+    <NavigationContainer>
+      <Home/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
