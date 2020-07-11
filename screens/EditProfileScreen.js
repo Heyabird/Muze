79e09ae3b52a4644ce05c40c268151637c399375
heya <heyabird@gmail.com>
2020-07-11 13:55:49 -0700

    move header to the top of screen for account, edit profile, and preferences

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -23,7 +23,7 @@ export default function EditProfileScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
