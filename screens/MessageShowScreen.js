cea29d7305136e6c974c465380b2a686af712309
heya <heyabird@gmail.com>
2020-07-11 13:54:48 -0700

    put basic elements in message show screen

--- screens/MessageShowScreen.js
+++ screens/MessageShowScreen.js
@@ -16,7 +16,7 @@ export default function Home() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
